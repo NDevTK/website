934837207ae67a8d3c2bd6ef1c3653122ab2c597
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-30 17:03:34 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -3,6 +3,9 @@ if (window.origin !== "null") {
     let f = document.createElement("iframe");
     f.sandbox = "allow-scripts";
     f.src = window.location.href;
+    f.addEventListener('popstate', function (event) {
+        console.log(event);
+    });
     f.style = "width: 100%; height: 100%;";
     window.document.body.appendChild(f);
     throw "No sandbox";
