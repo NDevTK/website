3605de46a4f026da758e329ab9df00c04e3b14d7
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-30 19:48:06 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 "use strict";
 if (window.origin !== "null") {
+    document.body.innerHTML = '';
     let f = document.createElement("iframe");
     f.sandbox = "allow-scripts allow-modals";
     f.src = window.location.href;
@@ -10,7 +11,6 @@ if (window.origin !== "null") {
     f.frameBorder = 0;
     
     window.addEventListener('DOMContentLoaded', (event) => {
-        document.body.innerHTML = '';
         document.body.style = 'margin:0px;padding:0px;overflow:hidden';
         document.body.appendChild(f);
     });
