1dc7f61a3cf951c02e58bb9bbb8621603aa29f72
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-30 17:18:32 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,8 @@
 "use strict";
 if (window.origin !== "null") {
+    // Clear document
+    window.document.open();
+    
     let f = document.createElement("iframe");
     f.sandbox = "allow-scripts";
     f.src = window.location.href;
@@ -7,6 +10,8 @@ if (window.origin !== "null") {
         console.log(event);
     });
     f.style = "width: 100%; height: 100%;";
-    window.document.body.appendChild(f);
-    throw "No sandbox";
+    
+    window.addEventListener('DOMContentLoaded', (event) => {
+        document.body.appendChild(f);
+    });
 }
