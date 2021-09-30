3ae01b1edfff33dd43be325cb9b31402242c2439
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-30 20:04:39 +0100

    Update and rename sandbox.js to sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+"use strict";
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request).then(inject));
+});
+
+function inject(response) {
+const headers = new Headers(response.headers);
+headers.set('Content-Security-Policy', 'sandbox allow-scripts allow-modals;');
+return new Response(response.body, { headers: headers });
+}
