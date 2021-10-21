6f03164eb52772ee7a831054d65afb77075c31fb
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-10-22 00:21:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ function inject(response) {
 const headers = new Headers(response.headers);
 headers.set('Content-Security-Policy', 'sandbox allow-scripts allow-modals allow-popups;');
 headers.set('X-Frame-Options', 'SAMEORIGIN');
-headers.set('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+headers.set('Cross-Origin-Opener-Policy', 'same-origin');
 headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
 headers.set('Strict-Transport-Security', 'max-age=31536000');
 headers.set('X-Content-Type-Options', 'nosniff')
