504995ff13b816e737115f8dee5e8193aba4fe21
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-12-03 03:13:40 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const headers = new Headers(response.headers);
 headers.set('Content-Security-Policy', 'sandbox allow-scripts allow-modals allow-popups;');
 headers.set('X-Frame-Options', 'SAMEORIGIN');
 headers.set('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
+//headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
 headers.set('Strict-Transport-Security', 'max-age=31536000');
 return new Response(response.body, { headers: headers });
 }
