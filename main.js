e9e1413479dad6f488392f801eeb58d40a8e5dd8
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-07-24 00:49:40 +0100

    LeGaCy

--- main.js
+++ main.js
@@ -30,25 +30,6 @@ function getRandom(max, min = 0) {
   return Math.random() * (max - min) + min;
 }
 
-window.onload = e => {
-  document.getElementById("rickroll").onclick = e => {
-    e.preventDefault();
-    googleAnimation();
-  }
-};
-
-function googleAnimation() {
-let w = open("https://www.google.com/");
-const NGGYU = "We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give, never gonna give (Give you up) (Ooh) Never gonna give, never gonna give (Give you up) We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry".split(" ");
-let counter = 0;
-let loop = setInterval(_ => {
-  w.location = "https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v3_shadow-1-small.png&color=ffffdc00&psize=50&scale=4&text="+encodeURIComponent(NGGYU[counter]);
-  counter += 1;
-  if (w.closed) clearInterval(loop);
-  if (counter === NGGYU.length) counter = 0;
-}, 1000);
-}
-
 function popunder(url) {
   return new Promise(r => {
     if (agent.has("Chromium")) {
