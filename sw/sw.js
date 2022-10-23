fee2fdb5fb447e66fd88f65fe97f0aa409a0307c
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-10-23 19:24:09 +0100

    Update sw.js

--- sw/sw.js
+++ sw/sw.js
@@ -1,11 +1,4 @@
 "use strict";
 self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request).then(inject));
+  event.respondWith(new Promise(() => {}));
 });
-
-function inject(response) {
-const headers = new Headers(response.headers);
-//headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-//headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
-return new Response(response.body, { headers: headers });
-}
