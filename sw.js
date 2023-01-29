4f9db3014ee9b974ef27d549395c8eb98074b82d
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-01-29 16:40:41 +0000

    policyOrigin

--- sw.js
+++ sw.js
@@ -5,10 +5,19 @@ self.addEventListener('fetch', function (event) {
 
 function inject(response) {
 const headers = new Headers(response.headers);
-//headers.set('Content-Security-Policy', 'sandbox allow-top-navigation allow-scripts allow-modals allow-popups allow-presentation;');
+
+headers.set('Content-Security-Policy', 'sandbox allow-top-navigation allow-scripts allow-modals allow-popups allow-presentation;');
 //headers.set('X-Frame-Options', 'SAMEORIGIN');
 headers.set('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
 //headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
 headers.set('Strict-Transport-Security', 'max-age=31536000');
+
+const policyOrigin = new URL(response.url).pathname;
+switch (policyOrigin) {
+  case 'tabPiP':
+    headers.set('Content-Security-Policy', '');
+    break
+}
+
 return new Response(response.body, { headers: headers });
 }
