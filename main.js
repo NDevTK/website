e10429ad0c5abf0cc49f0ee80d2eead7ef01beeb
NDevTK <31563761+NDevTK@users.noreply.github.com>
2026-09-25 12:47:17 +0100

    Update icon source URL for penguin image

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ async function secret(key) {
     
     // The user got here by finding the value of hash or cheated.
     changeSnowflake('🐈');
-    icon.src = 'https://ndev.tk/penguin.webp';
+    icon.src = 'https://ndevtk.github.io/website/penguin.webp';
     about.innerText = 'Still ' + about.innerText;
 }
 
